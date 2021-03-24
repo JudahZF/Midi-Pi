7a03fd51d8066aab78e61eb862100b2d45826e6b
Judah Fuller <judahfuller@gmail.com>
2021-03-24 18:40:13 +0000

    Added midi CC, PC and read

--- main.py
+++ main.py
@@ -9,12 +9,26 @@ while bootCheck < 8:
     bootCheck+=1
 
 midiOUT = UART(1,31250)
+midiIN = UART(2,31250)
 #Footswitch Var template Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
-def sendMidi(note):
+def sendMidiNote(note):
     activeLED.value(1)
     midiOUT.write(ustruct.pack("bbb",0x90,note,127))
     time.sleep(0.5)
     activeLED.value(0)
     time.sleep(0.5)
     midiOUT.write(ustruct.pack("bbb",0x80,note,0))
+
+def sendMidiCC(program, value):
+    activeLED.value(1)
+    time.sleep(0.5)
+    midiOUT.write(b"\xb0" + program.to_bytes(1, "big") + value.to_bytes(1, "big"))
+    time.sleep(0.5)
+    activeLED.value(0)
+
+def sendMidiPC(program):
+    midiOUT.write(b"\xc0" + program.to_bytes(1, "big"))
+
+def midiRead():
+    return midiIN.read()
