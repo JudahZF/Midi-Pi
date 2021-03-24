df3628ef418f9f071ab2e09f96155506624296b8
Judah Fuller <judahfuller@gmail.com>
2021-03-24 18:29:25 +0000

    Set up for sending midi notes

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from machine import Pin
-import time
+from machine import *
+import time, ustruct, ujson
+#Show blink LED to confirm Sucessful Boot
+activeLED = Pin(25, Pin.OUT)
+bootCheck = 0
+while bootCheck < 8:
+    activeLED.toggle()
+    time.sleep_ms(500)
+    bootCheck+=1
 
-pin = Pin(25, Pin.OUT)
+midiOUT = UART(1,31250)
+#Footswitch Var template Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
-while True:
-    pin.toggle()
-    time.sleep_ms(1000)
+def sendMidi(note):
+    activeLED.value(1)
+    midiOUT.write(ustruct.pack("bbb",0x90,note,127))
+    time.sleep(0.5)
+    activeLED.value(0)
+    time.sleep(0.5)
+    midiOUT.write(ustruct.pack("bbb",0x80,note,0))
